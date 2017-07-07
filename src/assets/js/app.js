645154fa17241a606eaf9ce25463c66c8392d5ad
Betsi Loayza <betsi18_12@hotmail.com>
2017-07-07 17:35:15 -0500

    principal

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,22 +1,26 @@
-// const render = (root) => {
-//     root.empty();
-//     const container = $('<div class=""></div>');
-//     container.append(Header());
-//     root.append(container);
-// }
-//
-// const state = {
-//     dataNews: null,
-//     imag: null,
-//     informacion: null,
-//     descripcion: null
-// };
-//
-// $(_ => {
-//  $.get("api/news/", function(data, status){
-//     state.dataNews=data;
-//     console.log(data);
-//     const root = $('.root');
-//     render(root);
-//  });
-// });
+const render = (root) => {
+    root.empty();
+    const container = $('<div class=""></div>');
+    container.append(Header());
+    container.append(NavBar());
+    container.append(Principal());
+    container.append(Row1());
+
+    root.append(container);
+}
+
+const state = {
+    dataNews: null,
+    imag: null,
+    informacion: null,
+    descripcion: null
+};
+
+$(_ => {
+ $.get("api/news/", function(data, status){
+    state.dataNews=data;
+    console.log(data);
+    const root = $('.root');
+    render(root);
+ });
+});
--- src/assets/js/components/header.js
+++ src/assets/js/components/header.js
@@ -1,3 +1,79 @@
-// $(_ => {
-// console.log(loadNews.news);
-// });
+
+const Header = () =>{
+  const top = $('<div class="hidden-xs center height-header"></div>');
+  const img = $('<img src="assets/img/logoicon.png" alt="">');
+  top.append(img);
+  return top;
+}
+
+const NavBar = () =>{
+  const navbarlarge = $('<nav class="navbar navbar-default"></nav>');
+  const container = $('<div class="container-fluid"></div>');
+  const barHeader = $('<div class="navbar-header"></div>');
+    const button = $('<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"></button>');
+    const span = $('<span class="sr-only">Toggle navigation</span>');
+    const span1 = $('<span class="icon-bar"></span>');
+    const span2 = $('<span class="icon-bar"></span>');
+    const span3 = $('<span class="icon-bar"></span>');
+  const logoXs = $('<div class="navbar-brand visible-xs" href="#"></div>');
+    const imagen = $('<img src="assets/img/logoicon.png" alt="">');
+  const collapse = $('<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"></div>');
+    const ul = $('<ul class="nav navbar-nav"></ul>');
+    const li1 = $('<li><a href="#">Lo Último <span class="sr-only">(current)</span></a></li>');
+    const li2 = $('<li><a href="#">Opinión</a></li>');
+    const li3 = $('<li><a href="#">Cultura</a></li>');
+    const li4 = $('<li><a href="#">Perú</a></li>');
+    const li5 = $('<li><a href="#">Tecnología</a></li>');
+    const li6 = $('<li><a href="#">Mundo</a></li>');
+    const li7 = $('<li><a href="#">Economía</a></li>');
+    const li8 = $('<li><a href="#">LifeStyle</a></li>');
+    const li9 = $('<li><a href="#">Deporte</a></li>');
+  navbarlarge.append(container);
+  container.append(barHeader);
+  barHeader.append(button);
+  button.append(span);
+  button.append(span1);
+  button.append(span2);
+  button.append(span3);
+  barHeader.append(logoXs);
+  logoXs.append(imagen);
+  container.append(collapse);
+  collapse.append(ul);
+  ul.append(li1, li2, li3, li4, li5, li6, li7, li8, li9);
+  return navbarlarge;
+}
+
+const Principal = () =>{
+  const principal = $('<div class="principal"></div>');
+  const divVisible = $('<div class="visible-xs"></div>');
+  const h2 = $('<h2>LO ÚLTIMO</h2>');
+  const hr = $('<hr>');
+  const imagen = $('<img src="assets/img/'+state.dataNews[0].img+'" alt="">');
+  const textoNew = $('<div class="text-news"></div>');
+  const h1 = $('<h1>'+state.dataNews[0].title+'</h1>');
+  const p = $('<p class="descripcion hidden-xs">'+state.dataNews[0].brief+'</p>');
+
+  principal.append(divVisible);
+  divVisible.append(h2, hr);
+  principal.append(imagen);
+  principal.append(textoNew);
+  textoNew.append(h1, p);
+  return principal;
+}
+
+const Row1 = () =>{
+  const row = $('<div class="row"></div>');
+  const column = $('<div class="col-xs-12 col-md-6 col-lg-6"></div>');
+  const imagen = $('<img src="assets/img/'+state.dataNews[1].img+'" alt="">')
+  const boxPlomo = $('<div class="info-plomo"></div>');
+  const h2 = $('<p>'+state.dataNews[1].title+'</p>');
+  const column2 = $('<div class="col-xs-12 col-md-3 col-lg-3"></div>');
+
+  const column3 = $('<div class="col-xs-12 col-md-3 col-lg-3"></div>');
+
+  row.append(column);
+  column.append(imagen);
+  column.append(boxPlomo);
+  boxPlomo.append(h2);
+  return row;
+}
